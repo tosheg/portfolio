62e04376db4b86d222334df49ccf079f7e232408
tosheg <49980028+tosheg@users.noreply.github.com>
2024-05-22 19:51:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('scroll', function() {
+document.addEventListener('scroll', debounce(function() {
     var parallaxElements = document.getElementsByClassName('parallax');
     var scrollTop = window.pageYOffset;
     Array.prototype.forEach.call(parallaxElements, function(el) {
@@ -6,4 +6,12 @@ document.addEventListener('scroll', function() {
         var offset = scrollTop * speed;
         el.style.transform = 'translate3d(0, ' + offset + 'px, 0)';
     });
-});
+}, 10));
+
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
